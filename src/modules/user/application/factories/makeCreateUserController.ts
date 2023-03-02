63f91de61e8118a6d50d91c30b196ba7dbacfd75
hugomos <vitor_osantos@hotmail.com>
2023-03-02 16:46:01 -0300

    feat: creating factory for user creation controller. @hugomos

---
+++ src/modules/user/application/factories/makeCreateUserController.ts
@@ -0,0 +1,13 @@
+import { InMemoryUserRepository } from 'infra/database/inMemory/inMemoryUserRepository'
+import { CreateUserController } from '../useCases/createUser/CreateUserController'
+import { CreateUserUseCase } from '../useCases/createUser/CreateUserUseCase'
+
+export class MakeCreateUserController {
+  constructor() { }
+
+  static make(): CreateUserController {
+    const inMemoryUserRepository = new InMemoryUserRepository()
+    const createUserUseCase = new CreateUserUseCase(inMemoryUserRepository)
+    return new CreateUserController(createUserUseCase)
+  }
+}
